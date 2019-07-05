31f8e01b701b24b853f306ff1ba057a212c965de
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-07-05 18:23:17 +0300

    Add login logic

--- android/app/src/main/java/com/stemwomanfrontend/MainActivity.java
+++ android/app/src/main/java/com/stemwomanfrontend/MainActivity.java
@@ -1,4 +1,4 @@
-package com.stemwomanfrontend;
+package com.chepkeitany.stemwoman;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "StemWomanFrontend";
+        return "StemWoman";
     }
 
     @Override
